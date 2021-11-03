deb3afdc30d8deaedb6ef526f4ea502c954df1db
Shanshan-Xia <xssilovexo@gmail.com>
2021-11-03 10:30:24 +0100

    Hausaufgaben

---
+++ code/task-01/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("pink");
+  stroke(225,0,0);
+  strokeWeight(6);
+  point(50,50);
+  stroke("blue");
+  strokeWeight(3);
+  fill("red");
+  circle(200,200,100);
+  ellipse(100,100,20,20);
+  stroke("yellow");
+  strokeWeight(3);
+  line(50,50,100,100);
+  square(150,150,50);
+  rect(200,200,50,10);
+}
+
